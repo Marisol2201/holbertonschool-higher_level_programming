b0a8120e6ade07aaa3fae3bd21004b40f51715bb
Marisol2201 <1529@holbertonschool.com>
2020-04-30 22:15:54 +0000

    task2 v0

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
+"""
+only is needed argv module from sys
+length = quantity of arguments
+(length - 1) //without argument 0
+for arg in range(1, length) //arg = argv position >0'
+    """
 if __name__ == "__main__":
-    from sys import argv  '#only is needed argv module from sys'
-    length = len(argv)  '#length = quantity of arguments'
+    from sys import argv
+    length = len(argv)
     if length == 1:
         print("0 arguments.")
     elif length == 2:
         print("1 argument:\n1: {:s}".format(argv[1]))
     else:
-        print("{:d} arguments:".format(length - 1))  '#without argument 0'
-        for arg in range(1, length):  '#arg = argv position >0'
+        print("{:d} arguments:".format(length - 1))
+        for arg in range(1, length):
             print("{:d}: {:s}".format(arg, argv[arg]))
