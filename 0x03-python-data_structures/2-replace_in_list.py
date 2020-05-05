136a821489b44d83846b0390810b943d602eb457
Marisol2201 <1529@holbertonschool.com>
2020-05-05 18:25:59 +0000

    create new_list was not necesary

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-        new_list = my_list
         if idx >= (len(my_list)):
             return(element)
         elif idx < 0:
             return(element)
         else:
             my_list[idx] = element
-            return(new_list)
             return(my_list)
