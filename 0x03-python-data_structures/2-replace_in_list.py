b7ecb0b4906d7f436472356b2ecea87e3a4ea952
Marisol2201 <1529@holbertonschool.com>
2020-05-05 18:38:12 +0000

    return changed (my_list against element)

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if (my_list is None) or (idx is None):
-        return
     if idx >= (len(my_list)):
-            return(element)
+            return(my_list)
     elif idx < 0:
-            return(element)
+            return(my_list)
     else:
             my_list[idx] = element
             return(my_list)
