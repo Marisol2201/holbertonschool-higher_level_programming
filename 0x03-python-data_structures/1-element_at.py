f18c8e2c1d272f041a09e5123ae349e7a06999db
Marisol2201 <1529@holbertonschool.com>
2020-05-05 17:13:23 +0000

    if idx >= (len(my_list))

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     for element in my_list:
-        if idx > (len(my_list)):
-            return("")
+        if idx >= (len(my_list)):
+            return
         elif idx < 0:
-            return("")
+            return
         else:
             if idx == element - 1:
                 return(element)
