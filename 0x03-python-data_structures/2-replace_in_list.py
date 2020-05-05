579dcfd72051dd09285fd664501917f0f82001a0
Marisol2201 <1529@holbertonschool.com>
2020-05-05 18:22:49 +0000

    without for because it was made with element in my_list(prototype)

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    for element in my_list:
+        new_list = my_list
         if idx >= (len(my_list)):
             return(element)
         elif idx < 0:
             return(element)
         else:
-            if idx == element - 1:
-                return(element)
+            my_list[idx] = element
+            return(new_list)
+            return(my_list)
