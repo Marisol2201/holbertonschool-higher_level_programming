fbcf83bf0db5aeff49f461d21387ab2b1399993c
Marisol2201 <1529@holbertonschool.com>
2020-05-06 00:54:35 +0000

    return 0 for length and None for string

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence is None:
-            return
+            return(0, None)
     for letter in sentence:
         return(len(sentence), letter)
