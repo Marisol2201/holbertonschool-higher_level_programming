fb62d672f9d1577f0e8ebce191c1ee294b853e6b
Marisol2201 <1529@holbertonschool.com>
2020-05-06 03:33:29 +0000

    condition for len < 1

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if (my_list is None) or (len(my_list) < 1):
-        return
+        return (None)
     big = 0
     for num in my_list:
         if num > big:
             big = num
-    return big
+    return (big)
