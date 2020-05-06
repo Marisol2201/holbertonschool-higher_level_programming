da65ff949da41221e8df82a0837375537cfd461a
Marisol2201 <1529@holbertonschool.com>
2020-05-06 01:10:37 +0000

    if sentence is

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    if sentence is None:
+    if sentence is "":
         return(0, None)
     else:
         return(length, sentence[0])
