f53aa0116f6209852569792974af9dfb0b9b2f04
Marisol2201 <1529@holbertonschool.com>
2020-05-06 20:07:34 +0000

    case when matriz is empty

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in range(len(matrix)):
-        for column in range(len(matrix[row])):
-            if (column < len(matrix[row]) - 1):
-                print("{}".format(matrix[row][column]), end=" ")
-            else:
-                print("{}".format(matrix[row][column]))
+    if matrix is None:
+        return
+    else:
+        for row in range(len(matrix)):
+            for column in range(len(matrix[row])):
+                if (column < len(matrix[row]) - 1):
+                    print("{}".format(matrix[row][column]), end=" ")
+                else:
+                    print("{}".format(matrix[row][column]))
