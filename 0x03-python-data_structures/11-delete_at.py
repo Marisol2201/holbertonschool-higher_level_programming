6d376cbc9e3875f3922f793152603537029e4443
Marisol2201 <1529@holbertonschool.com>
2020-05-07 00:48:20 +0000

    when a element of my list is equal to position

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    for idx in my_list:
         if idx >= (len(my_list)):
-            return(my_list)
+                return(my_list)
         elif idx < 0:
                 return(my_list)
         else:
-            del(my_list[idx])
-            return(my_list)
+            for element in range(len(my_list)):
+                if element == idx:
+                    del (my_list[idx])
+                    return(my_list)
