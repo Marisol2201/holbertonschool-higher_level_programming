005e9357c1c974ab86192f4cb0b1dbd7d1394d8b
Marisol2201 <1529@holbertonschool.com>
2020-05-10 22:19:07 +0000

    result starts with 0.0

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list is None:
+    if not my_list:
         return 0
-    result = 0.0
     mul = tuple([x * y for x, y in my_list[:]])
     num2 = tuple([y for x, y in my_list[:]])
     result = sum(mul[:]) / sum(num2[:])
