d79351397489de6573cc507f2e2946a5b4a5d84c
Marisol2201 <1529@holbertonschool.com>
2020-05-10 22:49:37 +0000

    lambda inside lambda

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    '''
-    The original argument is not modified
-    '''
     return list(map(lambda row: (list(map(lambda x: x * x, row))), matrix))
