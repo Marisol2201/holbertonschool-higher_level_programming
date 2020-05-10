d7e10b10f7bb66670d2307a32728f59e8be5ebda
Marisol2201 <1529@holbertonschool.com>
2020-05-10 17:28:35 +0000

    key in the loop

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,6 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    value = max(a_dictionary.keys(), default=None)
-    return(value)
+    for key in a_dictionary:
+        key = max(a_dictionary.keys(), default=None)
+        return(key)
