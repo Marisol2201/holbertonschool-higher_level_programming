d7ed63c63667c61b8b5065b6a6e77c914479e08b
Marisol2201 <1529@holbertonschool.com>
2020-05-10 22:24:29 +0000

    new variable and * the same num

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    new = [list(map(lambda x: x ** 2, row)) for row in matrix]
+    new = [list(map(lambda x: x * x, row)) for row in matrix]
     return new
