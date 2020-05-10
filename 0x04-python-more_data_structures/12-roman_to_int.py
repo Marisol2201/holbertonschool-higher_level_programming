91c93324a7447c481f164b65c79dd76aa8df5336
Marisol2201 <1529@holbertonschool.com>
2020-05-10 19:46:46 +0000

    name num to n

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    if type(roman_string) is not str or roman_string is None:
-        return None
-    int = 0
-    for num in range(0, len(roman_string)):
-        if num == 0 or dict[roman_string[num]] <= dict[roman_string[num - 1]]:
-            int += dict[roman_string[num]]
-        else:
-            int += dict[roman_string[num]] - 2 * dict[roman_string[num - 1]]
-    return int
+    if roman_string is None:
+        return 0
+    if type(roman_string) is str:
+        int = 0
+        for n in range(0, len(roman_string)):
+            if n == 0 or dict[roman_string[n]] <= dict[roman_string[n - 1]]:
+                int += dict[roman_string[n]]
+            else:
+                int += dict[roman_string[n]] - 2 * dict[roman_string[n - 1]]
+        return int
