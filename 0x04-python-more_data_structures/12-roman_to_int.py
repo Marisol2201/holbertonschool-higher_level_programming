82896644e5e863273cf58c46034390e2d242eb3c
Marisol2201 <1529@holbertonschool.com>
2020-05-10 18:46:38 +0000

    task12 v0

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    int = 0
+    for num in range(0, len(roman_string)):
+        if num == 0 or dict[roman_string[num]] <= dict[roman_string[num - 1]]:
+            int += dict[roman_string[num]]
+        else:
+            int += dict[roman_string[num]] - 2 * dict[roman_string[num - 1]]
+    return int
