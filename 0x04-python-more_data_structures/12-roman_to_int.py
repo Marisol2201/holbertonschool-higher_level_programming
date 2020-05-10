be986387db07df18dedd43c542d7f26ed879bf6c
Marisol2201 <1529@holbertonschool.com>
2020-05-10 18:51:47 +0000

    condition if is None

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    if type(roman_string) is not str or roman_string is None:
+        return None
     int = 0
     for num in range(0, len(roman_string)):
         if num == 0 or dict[roman_string[num]] <= dict[roman_string[num - 1]]:
