8de697f4e03e044ef39cebb971bac66a00b04507
Marisol2201 <1529@holbertonschool.com>
2020-05-10 22:11:08 +0000

    two basic operations

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list is None:
+        return 0
+    mul = tuple([x * y for x, y in my_list[:]])
+    num2 = tuple([y for x, y in my_list[:]])
+    result = sum(mul[:]) / sum(num2[:])
+    return(result)
