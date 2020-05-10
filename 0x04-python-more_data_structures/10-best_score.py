3ea4add989e51ad5db41d97dbd867e66b963d0a3
Marisol2201 <1529@holbertonschool.com>
2020-05-10 17:24:28 +0000

    with max value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,5 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    value = max(a_dictionary.keys())
+    value = max(a_dictionary.keys(), default=None)
     return(value)
