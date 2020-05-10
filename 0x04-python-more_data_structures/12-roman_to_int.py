a19614892fd71e60f6670adf4f51a5d5da82a83c
Marisol2201 <1529@holbertonschool.com>
2020-05-10 20:54:37 +0000

    another way

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    '''
+    num -1 (si hay 2 letras, la primera letra)
+    '''
     dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    if roman_string is None:
+    if type(roman_string) is not str or roman_string is None:
         return 0
-    if type(roman_string) is str:
-        int = 0
-        for n in range(0, len(roman_string)):
-            if n == 0 or dict[roman_string[n]] <= dict[roman_string[n - 1]]:
-                int += dict[roman_string[n]]
-            else:
-                int += dict[roman_string[n]] - 2 * dict[roman_string[n - 1]]
-        return int
+    int = 0
+    temp = roman_string
+    for num in range(len(temp)):
+        if num > 0 and dict[temp[num]] > dict[temp[num - 1]]:
+            int += dict[temp[num]] - 2 * dict[temp[num - 1]]
+        else:
+            int += dict[temp[num]]
+    return int
