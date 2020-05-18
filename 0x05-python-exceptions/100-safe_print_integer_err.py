282f3650bd85773fede89d5511d552074de6bd42
Marisol2201 <1529@holbertonschool.com>
2020-05-18 23:38:47 +0000

    ValueError instead Exception

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,6 +3,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return(True)
-    except Exception as error_msn:
+    except (ValueError) as error_msn:
         print("Exception: {}".format(error_msn), file=sys.stderr)
         return(False)
