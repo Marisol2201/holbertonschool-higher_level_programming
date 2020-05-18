ca9eccb686a3084e799d7d390daf20b570220d36
Marisol2201 <1529@holbertonschool.com>
2020-05-18 23:32:18 +0000

    stderr in a file

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,8 @@
 def safe_print_integer_err(value):
-    from sys import stderr
+    import sys
     try:
         print("{:d}".format(value))
         return(True)
     except Exception as error_msn:
-        stderr.write("Exception: {}\n".format(error_msn))
+        print("Exception: {}".format(error_msn), file=sys.stderr)
         return(False)
