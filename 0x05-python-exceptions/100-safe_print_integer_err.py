48fad3a66e4ea67804c9754f58a83410a6828710
Marisol2201 <1529@holbertonschool.com>
2020-05-18 23:07:51 +0000

    without msn saved in a variable

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,7 +3,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return(True)
-    except Exception as e:
-        msn = "Exception: Unknown format code 'd' for object of type 'str'\n"
-        sys.stderr.write(msn)
+    except Exception as error_msn:
+        sys.stderr.write("Exception: {}\n".format(error_msn))
         return(False)
