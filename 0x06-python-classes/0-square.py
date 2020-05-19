986acb80ee9e5ede20ab8f897eeb07e2177cc90c
Marisol2201 <1529@holbertonschool.com>
2020-05-19 21:31:25 +0000

    task0 v1 with comments with ''

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Empty class Square that defines a square"""
+'''Empty class Square that defines a square'''
 
 
 class Square:
-    """Contains nothing"""
+    '''Contains nothing'''
     pass
