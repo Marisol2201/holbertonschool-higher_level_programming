dadf33a3edc36b7414edb7771c8b28392bcb36a4
Marisol2201 <1529@holbertonschool.com>
2020-05-19 00:52:55 +0000

    shebang

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,9 @@
-import sys
-
-
+#!/usr/bin/python3
 def safe_print_integer_err(value):
+    import sys
     try:
         print("{:d}".format(value))
         return True
-    except (TypeError, ValueError) as e:
-        print("Exception: {}".format(e), file=sys.stderr)
+    except Exception as excep:
+        print("Exception {}".format(excep), file=sys.stderr)
         return False
