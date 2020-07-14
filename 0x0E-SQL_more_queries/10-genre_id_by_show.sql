e92d4fa5e8584a1b025f9ae59df3f1301e476a1d
Marisol2201 <1529@holbertonschool.com>
2020-07-14 22:57:14 +0000

    adding a new comment

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,5 @@
 -- Import the database dump from hbtn_0d_tvshows to your MySQL server
+-- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
 -- 0)echo "CREATE DATABASE hbtn_0d_tvshows;" | mysql -uroot -p (it was at the begining of the project)
 -- 1) bring the file to the repo
 -- 2) mysql -u root -p hbtn_0d_tvshows < hbtn_0d_tvshows.sql (it was in the documentation)
