0b04c0f699712e0efeb27989ab75fd22a9f7a945
Marisol2201 <1529@holbertonschool.com>
2020-08-08 20:23:29 +0000

    first method

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""prints the first State object from the database hbtn_0e_6_usa"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    """Your code should not be executed when imported"""
+
+    # This engine just used to query for list of databases
+    mysql_engine = create_engine('mysql://{}:{}@{}:{}/{}'.format(
+        sys.argv[1], sys.argv[2],
+        'localhost', 3306, sys.argv[3]))
+
+    # create a configured "Session" class and create a Session
+    Session = sessionmaker(bind=mysql_engine)
+    session = Session()
+
+    state = session.query(State).order_by(State.id).first()
+    if state:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("")
