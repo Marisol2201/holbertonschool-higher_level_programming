7fbc75b6bb2c4ece74cf3d586855131d93bd4679
Marisol2201 <1529@holbertonschool.com>
2020-08-18 21:38:36 +0000

    without -I flag

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends request to a URL passed as arg & displays only status code of response
-curl -s -o /dev/null -I -w "%{http_code}" "$1" ; echo ""
+curl -s -o /dev/null -w "%{http_code}" "$1" ; echo ""
