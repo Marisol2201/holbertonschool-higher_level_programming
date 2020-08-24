6e3a9d7000c28119e15623a565b570b56d0c0d99
Marisol2201 <1529@holbertonschool.com>
2020-08-24 21:46:00 +0000

    task6 v0

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" takes in a URL, sends a request to the URL and displays the value of the
+variable X-Request-Id in the response header"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    """Your code should not be executed when imported"""
+    url = argv[1]
+    email = argv[2]
+    payload = {'email': email}
+    r = requests.post(url, params=payload)
+    print(r.text)
