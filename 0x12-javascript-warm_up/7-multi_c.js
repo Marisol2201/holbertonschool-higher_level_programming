bae70cd7daaabe9f33bd929069fd2551f9d11b35
Marisol2201 <1529@holbertonschool.com>
2020-09-15 22:49:41 +0000

    spaces fixed

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,7 +3,7 @@ let i = 0;
 const num = Number(process.argv[2]);
 if (num) {
   for (; i < num; i++) {
-  console.log('C is fun');
+    console.log('C is fun');
   }
 } else {
   console.log('Missing number of occurrences');
