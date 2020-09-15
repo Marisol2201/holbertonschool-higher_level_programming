bf90749a178b2c912ea5a8ed20b3eb348c1bd3c5
Marisol2201 <1529@holbertonschool.com>
2020-09-15 21:08:58 +0000

    with concatenation of strings

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
 const args = process.argv;
+const string1 = 'c'
+const string2 = 'is'
+const string3 = 'cool'
+const string4 = 'undefined'
 if (args.length > 3) {
-  console.log('c is cool');
+  console.log(string1 + ' ' + string2 + ' ' + string3);
 } else if (args.length === 3) {
-  console.log('c is undefined');
+  console.log(string1 + ' ' + string2 + ' ' + string4);
 } else {
-  console.log('undefined is undefined');
+  console.log(string4 + ' ' + string2 + ' ' + string4);
 }
