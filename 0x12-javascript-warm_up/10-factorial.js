f1e9c27f13d03b0a7340687e5c0ba58696cffba0
Marisol2201 <1529@holbertonschool.com>
2020-09-15 23:59:41 +0000

    recursivefactorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const num = process.argv[2];
-function factorial (n) {
-  var total = 1;
-  for (let i = 1; i <= n; i++) {
-    total = total * i;
-  }
-  return total;
+function factorialRecursivo (n) { 
+	if (n == 0){ 
+		return 1; 
+	}
+	return n * factorialRecursivo (n-1); 
 }
-console.log(factorial(parseInt(num)));
+console.log(factorialRecursivo(parseInt(num)));
