cb8ccb57f00cee1d67b80e85f7aaf40b9ce43568
Marisol2201 <1529@holbertonschool.com>
2020-09-16 00:04:47 +0000

    semistandard fixed

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const num = process.argv[2];
-function factorialRecursivo (n) { 
-	if (n == 0){ 
-		return 1; 
-	}
-	return n * factorialRecursivo (n-1); 
+function factorialRecursivo (n) {
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorialRecursivo(n - 1);
 }
 console.log(factorialRecursivo(parseInt(num)));
