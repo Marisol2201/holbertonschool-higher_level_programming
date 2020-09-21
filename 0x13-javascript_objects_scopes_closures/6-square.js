ca611e7e237746229f08f672c5faef6e68410192
Marisol2201 <1529@holbertonschool.com>
2020-09-21 22:31:57 +0000

    tasks0 to 10 v0

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const Sqr = require('./5-square');
+module.exports = class Square extends Sqr {
+  charPrint (c) {
+    if (c === undefined) super.print();
+    else {
+      for (let i = 0; i < this.height; i++) {
+        console.log('c'.repeat(this.width));
+      }
+    }
+  }
+};
