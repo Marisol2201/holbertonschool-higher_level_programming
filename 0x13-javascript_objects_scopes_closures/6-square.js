e9f1b4a209de818c5e192d050e49a688aaaee96e
Marisol2201 <1529@holbertonschool.com>
2020-09-21 22:41:54 +0000

    this.width instead of this.height

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,7 +4,7 @@ module.exports = class Square extends Sqr {
   charPrint (c) {
     if (c === undefined) super.print();
     else {
-      for (let i = 0; i < this.height; i++) {
+      for (let i = 0; i < this.width; i++) {
         console.log('C'.repeat(this.width));
       }
     }
