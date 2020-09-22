9252de2d28cd27666b3e261d7a935a74dae1c8b0
Marisol2201 <1529@holbertonschool.com>
2020-09-22 18:16:45 +0000

    semistandard fixed

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 const request = require('request');
 request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, response, body) {
   if (err) {
-    console.log(err)
+    console.log(err);
   }
   const info = JSON.parse(body);
   console.log(info.title);
