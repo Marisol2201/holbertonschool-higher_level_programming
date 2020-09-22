b62ca83c090aa047a0e227ceee20d6a1ed1a2149
Marisol2201 <1529@holbertonschool.com>
2020-09-22 18:15:44 +0000

    url fixed

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
-request('http://swapi.co/api/films/' + process.argv[2] + '/', function (error, response, body) {
-  if (error) {
-    console.error('error:', error);
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, response, body) {
+  if (err) {
+    console.log(err)
   }
-  body = JSON.parse(body);
-  console.log(body.title);
+  const info = JSON.parse(body);
+  console.log(info.title);
 });
