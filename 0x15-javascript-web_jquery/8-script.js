94ee4bfe76386e6e3c1f8357ccce56fcf4d40daf
Marisol2201 <1529@holbertonschool.com>
2020-09-24 13:41:25 -0500

    url changed

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-$.get('https://swapi.co/api/films/?format=json', function (data) {
+$.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
   let movies = data.results;
   for (let i = 0; i < movies.length; i++) {
     $('ul#list_movies').append('<li>' + movies[i].title + '</li>');
